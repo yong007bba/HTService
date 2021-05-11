76d03f7a0df119fcc2855f38d1b90b09162ad6f5
hsw <52780262+DevaCoding@users.noreply.github.com>
2021-05-11 09:47:31 +0900

    create project

---
+++ htservice/src/main/java/com/springboot2/htservice/HtserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot2.htservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtserviceApplication.class, args);
+	}
+
+}
---
+++ htservice/src/test/java/com/springboot2/htservice/HtserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot2.htservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
